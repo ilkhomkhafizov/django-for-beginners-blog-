110108e7bfa48220ee2f3d63d2372df1b6d3ec3a
ilkhomkhafizov <ilhomhafizov6@gmail.ru>
2020-09-09 23:56:36 +0300

    added CRUD functionality

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+# from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Post
 
 
@@ -6,8 +8,30 @@ class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'all_lists'
+    
 
 
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
